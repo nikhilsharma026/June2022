5c5f16b886c202d86557833b7e8b2f3a68cd63de
Nikhil <nikhil.sharma@mavenwave.com>
2022-06-28 15:23:38 +0530

    First commit

---
+++ com.java.practise/src/com/java/practise/Strings.java
@@ -0,0 +1,23 @@
+package com.java.practise;
+
+public class Strings {
+	public static void main(String[] args) 
+	{
+		
+		String s= "124,56";
+		
+		String[] s1=s.split(",");
+		
+System.out.println(s1[0]);
+		
+		System.out.println(s.charAt(3));
+
+String i=s.substring(0,3);
+	System.out.println(i);
+	
+
+
+	}
+		
+
+}
